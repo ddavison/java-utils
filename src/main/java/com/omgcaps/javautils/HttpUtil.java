7db58a8153979be1bd1d461bdce18969502472e1
ddavison <daniel.davison@proquest.com>
2013-04-07 03:52:38 -0400

    Adding Maven, and v1 of HttpUtil with test.

--- src/main/java/com/omgcaps/javautils/HttpUtil.java
+++ src/main/java/com/omgcaps/javautils/HttpUtil.java
@@ -1,31 +1,93 @@
 package com.omgcaps.javautils;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 
+import org.apache.commons.io.IOUtils;
+
+/**
+ * 
+ * HttpUtil delivers a nice utility for any HTTP request in Java.
+ * 
+ * @author omgcaps
+ * 
+ */
 public class HttpUtil {
+
+	private static HttpURLConnection connection = null;
 
 	/**
 	 * Perform a GET request.
 	 * 
 	 * @param url
 	 *            Url to execute the request.
-	 * @param parameters
+	 * @return Response from server.
+	 * @throws IOException
+	 */
+	public static String get(String url) throws IOException {
+		return get(url, null);
+	}
+
+	/**
+	 * Perform a GET request.
+	 * 
+	 * @param url
+	 *            Url to execute the request.
+	 * @param params
 	 *            Array of parameters to pass.
 	 * @return Response from server.
+	 * @throws IOException
 	 */
-	public static String get(String url, HashMap<String, String> parameters) {
-		return null;
+	public static String get(String url, HashMap<String, String> params)
+			throws IOException {
+		String newUrl;
+		boolean firstVar = true;
+
+		if (url == null)
+			return null;
+		newUrl = url;
+
+		if (params != null)
+			for (String getVar : params.keySet()) {
+				if (firstVar) {
+					newUrl += "?" + getVar + "=" + params.get(getVar);
+					firstVar = false;
+				}
+
+				newUrl += "&" + getVar + "=" + params.get(getVar);
+			}
+
+		return request("GET", newUrl, null, null);
 	}
 
 	/**
 	 * Perform a POST request.
 	 * 
 	 * @param url
-	 * @param parameters
-	 * @return
+	 *            Url to execute the request.
+	 * @param params
+	 *            Array of parameters to pass.
+	 * 
+	 * @return Response from server.
+	 * @throws IOException
+	 * 
 	 */
-	public static String post(String url, HashMap<String, String> parameters) {
-		return null;
+	public static String post(String url, HashMap<String, String> params)
+			throws IOException {
+
+		String body = "";
+
+		if (params != null)
+			for (String postVar : params.keySet())
+				body += postVar + "=" + params.get(postVar) + "&";
+
+		// remove the trailing &.
+		body = body.substring(0, (body.length() - 1));
+
+		return request("POST", url, body, null);
 	}
 
 	/**
@@ -33,24 +95,96 @@ public class HttpUtil {
 	 * 
 	 * @param url
 	 *            Url to execute the request.
-	 * @param parameters
+	 * @param params
 	 *            Array of parameters to pass.
 	 * @return Response from server.
+	 * @throws IOException
 	 */
-	public static String put(String url, HashMap<String, String> parameters) {
-		return null;
+	public static String put(String url, HashMap<String, String> params)
+			throws IOException {
+		String body = null;
+
+		if (params != null) {
+			body = "";
+			for (String putVar : params.keySet()) {
+				body += putVar + "=" + params.get(putVar) + "&";
+			}
+		}
+
+		// remove the trailing &.
+		if (body != null)
+			body = body.substring(0, (body.length() - 1));
+
+		return request("PUT", url, body, null);
+	}
+
+	/**
+	 * Perform a DELETE request.
+	 * 
+	 * @param url
+	 *            Url to execute the request.
+	 * @param params
+	 *            Array of parameters to pass.
+	 * @return Response from server.
+	 * @throws IOException
+	 */
+	public static String delete(String url, HashMap<String, String> params)
+			throws IOException {
+		String body = null;
+
+		if (params != null) {
+			body = "";
+			for (String deleteVar : params.keySet()) {
+				body += deleteVar + "=" + params.get(deleteVar) + "&";
+			}
+		}
+
+		// remove the trailing &.
+		if (body != null)
+			body = body.substring(0, (body.length() - 1));
+
+		return request("DELETE", url, body, null);
 	}
 
 	/**
 	 * Perform a generic request.
 	 * 
+	 * @param method
+	 *            Request method.
 	 * @param url
 	 *            Url to execute the request.
-	 * @param parameters
-	 * @return
+	 * @param body
+	 *            Body of request.
+	 * @param headers
+	 *            Request headers.
+	 * @return Response from server.
+	 * @throws IOException
 	 */
-	private static boolean request(String url,
-			HashMap<String, String> parameters) {
-		return false;
+	public static String request(String method, String url, String body,
+			HashMap<String, String> params) throws IOException {
+
+		URL u = new URL(url);
+
+		connection = (HttpURLConnection) u.openConnection();
+
+		// Delete method does not support dooutput.
+		if (method != "DELETE")
+			connection.setDoOutput(true);
+		connection.setUseCaches(false);
+		connection.setRequestMethod(method);
+
+		if (body != null) {
+			DataOutputStream out = new DataOutputStream(
+					connection.getOutputStream());
+			out.writeBytes(body);
+		}
+
+		if (params != null)
+			for (String param : params.keySet())
+				connection.addRequestProperty(param, params.get(param));
+
+		connection.connect();
+
+		return IOUtils.toString(connection.getInputStream());
 	}
 }
---
+++ src/test/java/com/omgcaps/javautils/TestHttpUtil.java
@@ -0,0 +1,135 @@
+package com.omgcaps.javautils;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+public class TestHttpUtil {
+
+	final String testUrl = "http://omggamesftw.com/tools/httptest.php";
+	final String var1 = "variable1", var2 = "variable2", value = "true";
+
+	Response response;
+
+	@Test
+	public void testGetOverload() {
+		try {
+			String response = HttpUtil.get(testUrl + "?" + var1 + "=" + value
+					+ "&" + var2 + "=" + value);
+			this.response = new Gson().fromJson(response, Response.class);
+
+			assertEquals(this.response.getParameters().size(), 2);
+			assertEquals(this.response.getMethod(), "GET");
+			assertEquals(this.response.getParameters().get(var1), value);
+			assertEquals(this.response.getParameters().get(var2), value);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testGet() {
+		try {
+			HashMap<String, String> params = new HashMap<String, String>();
+
+			params.put(var1, value);
+			params.put(var2, value);
+
+			String response;
+			response = HttpUtil.get(testUrl, params);
+
+			this.response = new Gson().fromJson(response, Response.class);
+
+			assertEquals(this.response.getParameters().size(), 2);
+			assertEquals(this.response.getMethod(), "GET");
+			assertEquals(this.response.getParameters().get(var1), value);
+			assertEquals(this.response.getParameters().get(var2), value);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void testPost() {
+		try {
+			HashMap<String, String> params = new HashMap<String, String>();
+
+			params.put(var1, value);
+			params.put(var2, value);
+
+			String response;
+			response = HttpUtil.post(testUrl, params);
+
+			this.response = new Gson().fromJson(response, Response.class);
+
+			assertEquals(this.response.getParameters().size(), 2);
+			assertEquals(this.response.getMethod(), "POST");
+			assertEquals(this.response.getParameters().get(var1), value);
+			assertEquals(this.response.getParameters().get(var2), value);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testPut() {
+		try {
+			String response;
+			response = HttpUtil.put(testUrl, null);
+
+			this.response = new Gson().fromJson(response, Response.class);
+
+			assertEquals(this.response.getParameters().size(), 0);
+			assertEquals(this.response.getMethod(), "PUT");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testDelete() {
+		try {
+			String response;
+			response = HttpUtil.delete(testUrl, null);
+
+			this.response = new Gson().fromJson(response, Response.class);
+
+			assertEquals(this.response.getParameters().size(), 0);
+			assertEquals(this.response.getMethod(), "DELETE");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	class Response {
+		private String method;
+		private HashMap<String, String> parameters;
+
+		public String getMethod() {
+			return this.method;
+		}
+
+		public void setMethod(String method) {
+			this.method = method;
+		}
+
+		public HashMap<String, String> getParameters() {
+			return this.parameters;
+		}
+
+		public void setParameters(HashMap<String, String> parameters) {
+			this.parameters = parameters;
+		}
+	}
+}
