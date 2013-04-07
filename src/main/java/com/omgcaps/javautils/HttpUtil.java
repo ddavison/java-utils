f67d96bd96491cb997903cbf55a6926f5878db06
ddavison <daniel.davison@proquest.com>
2013-04-07 00:13:06 -0400

    Initial skeleton

--- src/main/java/com/omgcaps/javautils/HttpUtil.java
+++ src/main/java/com/omgcaps/javautils/HttpUtil.java
@@ -1,5 +1,56 @@
 package com.omgcaps.javautils;
 
+import java.util.HashMap;
+
 public class HttpUtil {
 
+	/**
+	 * Perform a GET request.
+	 * 
+	 * @param url
+	 *            Url to execute the request.
+	 * @param parameters
+	 *            Array of parameters to pass.
+	 * @return Response from server.
+	 */
+	public static String get(String url, HashMap<String, String> parameters) {
+		return null;
+	}
+
+	/**
+	 * Perform a POST request.
+	 * 
+	 * @param url
+	 * @param parameters
+	 * @return
+	 */
+	public static String post(String url, HashMap<String, String> parameters) {
+		return null;
+	}
+
+	/**
+	 * Perform a PUT request.
+	 * 
+	 * @param url
+	 *            Url to execute the request.
+	 * @param parameters
+	 *            Array of parameters to pass.
+	 * @return Response from server.
+	 */
+	public static String put(String url, HashMap<String, String> parameters) {
+		return null;
+	}
+
+	/**
+	 * Perform a generic request.
+	 * 
+	 * @param url
+	 *            Url to execute the request.
+	 * @param parameters
+	 * @return
+	 */
+	private static boolean request(String url,
+			HashMap<String, String> parameters) {
+		return false;
+	}
 }
